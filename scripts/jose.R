8b2b9c91400daaf4cd3d0cfed1f6f2c90e5cce98
JoseDGonzalezR <jd.gonzalezr12@uniandes.edu.co>
2023-10-25 00:37:15 -0500

    Visualizaciones, tratamiento de texto.

--- scripts/jose.R
+++ scripts/jose.R
@@ -3,7 +3,18 @@
 #Cargamos paquetes requeridos
 
 library(pacman)
-p_load(tidyverse, rio, tidymodels, glmnet, stargazer, dplyr)
+p_load(tidyverse, 
+       rio, 
+       tidymodels, 
+       glmnet, 
+       stargazer, 
+       dplyr, 
+       plotly, 
+       leaflet, 
+       rgeos, 
+       tmaptools, 
+       sf, 
+       osmdata)
 
 #Cargamos data-frames
 
@@ -63,16 +74,6 @@ distributionBathrooms()
 
 #Vemos que hay una distribución asimétrica y echada a la izquierda. Imputaremos con moda, al tomar valores enteros.
 
-distributionBedrooms <- function (bedrooms) {
-  N = length(df$bedrooms)
-  bedrooms <- na.omit(df$bedrooms)
-  hist( df$bedrooms,col = "light blue")
-}
-
-distributionBedrooms()
-
-#Vemos que hay una distribución asimétrica, y aunque más normalizada, sigue estando echada a la izquierda. Imputaremos con moda, al tomar valores enteros.
-
 
 ##Distribución variable dependiente
 
@@ -111,15 +112,17 @@ summary(df$precio_por_mt2) %>%
   mutate(V1 = scales::dollar(V1))
 
 #Vemos que hay un valor mínimo de $20,424 y uno máximo de $40,450,000 por metro cuadrado, lo cual, a todas luces es extraordinariamente alto.
-#Estableceremos un rango mínimo de $700,000 máximo de $15,000,000 por metro cuadrado (según estimaciones, por barrio, el metro cuadrado más caro de Bogotá ronda alrededor de $7,145,435, y el más barato, alrededor de $873.138. Por ello dejaremos un poco de espacio para evitar sesgo, pero sí para eliminar valores irreales que puedan perjudicar las predicciones)
+#Estableceremos un rango mínimo de $600,000 máximo de $15,000,000 por metro cuadrado (según estimaciones, por barrio, el metro cuadrado más caro de Bogotá ronda alrededor de $7,145,435, y el más barato, alrededor de $873.138. Por ello dejaremos un poco de espacio para evitar sesgo, pero sí para eliminar valores irreales que puedan perjudicar las predicciones)
 
 df <- df %>%
-  filter(between(precio_por_mt2, 700000,  15e6))
+  filter(between(precio_por_mt2, 600000,  15e6))
 
 #Visualizamos la nueva distribución de la variable dependiente.
 
 p <- ggplot(df, aes(x = price)) +
   geom_histogram(fill = "darkblue", alpha = 0.4) +
+  labs(x = "Valor de venta (log-scale)", y = "Cantidad") +
+  scale_x_log10(labels = scales::dollar) +
   theme_bw()
 
 p
@@ -168,16 +171,6 @@ distributionBathroomsTest()
 
 #Vemos que hay una distribución asimétrica, aunque algo más normalizada, pero echada a la izquierda. Imputaremos con mediana.
 
-distributionBedroomsTest <- function (bedrooms) {
-  N = length(df_test$bedrooms)
-  bedrooms <- na.omit(df_test$bedrooms)
-  hist( df_test$bedrooms,col = "light blue")
-}
-
-distributionBedroomsTest()
-
-#Vemos que hay una distribución asimétrica, aunque algo más normalizada, pero echada a la izquierda. Imputaremos con mediana.
-
 ##Imputación
 
 #Cálculos de modas y medianas
@@ -186,7 +179,6 @@ distributionBedroomsTest()
 
 df %>%count(rooms) %>% head() #La moda es 3
 df %>%count(bathrooms) %>% head() #La moda es 2
-df %>%count(bedrooms) %>% head() #La moda es 3
 
 mediana_sup_cubierta <- median(df$surface_covered, na.rm = TRUE) #108
 mediana_sup_total <- median(df$surface_total, na.rm = TRUE) #119
@@ -197,7 +189,6 @@ mediana_rooms <- median(df$rooms, na.rm = TRUE) #3
 
 df_test %>%count(rooms) %>% head() #La moda es 3
 df_test %>%count(bathrooms) %>% head() #La moda es 2
-df_test %>%count(bedrooms) %>% head() #La moda es 3
 
 mediana_sup_cubierta_test <- median(df_test$surface_covered, na.rm = TRUE) #118
 mediana_sup_total_test <- median(df_test$surface_total, na.rm = TRUE) #120
@@ -211,7 +202,6 @@ mediana_rooms_test <- median(df_test$rooms, na.rm = TRUE) #2
 df <- df %>%
   mutate(rooms = replace_na(rooms, 3),
          bathrooms = replace_na(bathrooms, 2),
-         bedrooms = replace_na(bedrooms, 3),
          surface_covered = replace_na(surface_covered, mediana_sup_cubierta),
          surface_total = replace_na(surface_total, mediana_sup_total),)
 
@@ -220,29 +210,197 @@ df <- df %>%
 df_test <- df_test %>%
   mutate(rooms = replace_na(rooms, 3),
          bathrooms = replace_na(bathrooms, 2),
-         bedrooms = replace_na(bedrooms, 3),
          surface_covered = replace_na(surface_covered, mediana_sup_cubierta_test),
          surface_total = replace_na(surface_total, mediana_sup_total_test),)
+
+##Tratamiento Datos Espaciales
+
+
+#Filtramos observaciones que no tengan latitud ni longitud
+
+df <- df %>%
+  filter(!is.na(lat) & !is.na(lon))
+
+df_test <- df_test %>%
+  filter(!is.na(lat) & !is.na(lon))
+
+##Visualización y tratamiento de datos espaciales
+
+#Visualizamos los datos
+
+leaflet() %>%
+  addTiles() %>%
+  addCircles(lng = df$lon,
+             lat = df$lat)
+
+leaflet() %>%
+  addTiles() %>%
+  addCircles(lng = df_test$lon,
+             lat = df_test$lat)
+
+#Establecemos los límites de los datos espaciales para que coincidan con los límites del área urbana de la ciudad de Bogotá, de acuerdo a como los establece OSM
+
+limites <- getbb("Bogotá Colombia")
+
+limites
+
+df <- df %>%
+  filter(
+    between(lon, limites[1, "min"], limites[1, "max"]) & 
+      between(lat, limites[2, "min"], limites[2, "max"])
+  )
+
+df_test <- df_test %>%
+  filter(
+    between(lon, limites[1, "min"], limites[1, "max"]) & 
+      between(lat, limites[2, "min"], limites[2, "max"])
+  )
+
+#Escalamos la variable de precio de metro cuadrado para mejorar visualización
+
+df <- df %>%
+  mutate(precio_por_mt2_sc =( (precio_por_mt2 - min(precio_por_mt2)) / (max(precio_por_mt2) - min(precio_por_mt2))))
+
+#Gráfica de Colores por precio
+
+df <- df %>%
+  mutate(color = case_when(precio_por_mt2 >= 13000000 ~ "#F32F0E",
+                           precio_por_mt2 >= 10000000 & precio_por_mt2_sc <= 12999999 ~ "#F3870E",
+                           precio_por_mt2 >= 8000000 & precio_por_mt2_sc <= 9999999 ~ "#F3E70E",
+                           precio_por_mt2 >= 6000000 & precio_por_mt2_sc <= 7999999 ~ "#AFF30E",
+                           precio_por_mt2 >= 4000000 & precio_por_mt2_sc <= 5999999 ~ "#0EF35E",
+                           precio_por_mt2 >= 3000000 & precio_por_mt2_sc <= 3999999 ~ "#0EF3CF",
+                           precio_por_mt2 >= 2000000 & precio_por_mt2_sc <= 2999999 ~ "#17B53E",
+                           precio_por_mt2 >= 1000000 & precio_por_mt2_sc <= 1999999 ~ "#0EC7F3",
+                           precio_por_mt2 >= 900000 & precio_por_mt2_sc <= 999999 ~ "#0E66F3",
+                           precio_por_mt2 >= 800000 & precio_por_mt2_sc <= 899999 ~ "#1E0EF3",
+                           precio_por_mt2 >= 7000000 & precio_por_mt2_sc <= 799999 ~ "#970EF3"))
+
+
+leaflet() %>%
+  addTiles() %>%
+  addCircles(lng = df$lon, 
+             lat = df$lat, 
+             col = df$color,
+             fillOpacity = 1,
+             opacity = 1,
+             radius = df$precio_por_mt2_sc*10)
+
+##Tratamiento de texto de la variable "description"
+
+
+#Normalización
+
+  #Pasamos todo a minúscula
+
+df <- df %>%
+  mutate(description = str_to_lower(description))
+
+df_test <- df_test %>%
+  mutate(description = str_to_lower(description))
+
+  #Eliminamos tildes
+
+df <- df %>%
+  mutate(description = iconv(description, from = "UTF-8", to = "ASCII//TRANSLIT"))
+
+df_test <- df_test %>%
+  mutate(description = iconv(description, from = "UTF-8", to = "ASCII//TRANSLIT"))
+
+  #Eliminamos caracteres especiales que no sean alfanuméricos
+
+df <- df %>%
+  mutate(description = str_replace_all(description, "[^[:alnum:]]", " "))
+
+df_test <- df_test %>%
+  mutate(description = str_replace_all(description, "[^[:alnum:]]", " "))
+
+  #Eliminamos espacios extras
+
+df <- df %>%
+  mutate(description = str_trim(gsub("\\s+", " ", description)))
+
+df_test <- df_test %>%
+  mutate(description = str_trim(gsub("\\s+", " ", description)))
+
+
+#Verificación variable property_type
+
+#train
+
+df <- df %>%
+  mutate(property_type_2 = ifelse(grepl("apto|apartamento|pent house", description), "apartamento", property_type))
+
+df <- df %>%
+  mutate(property_type_2 = ifelse(grepl("casa", description), "casa", property_type)) %>%
+  select(-property_type)
+
+#test
+
+df_test <- df_test %>%
+  mutate(property_type_2 = ifelse(grepl("apto|apartamento|pent house", description), "apartamento", property_type))
+
+df_test <- df_test %>%
+  mutate(property_type_2 = ifelse(grepl("casa", description), "casa", property_type)) %>%
+  select(-property_type)
+
+#Ahora podemos convertir a Dummy la variable property_type_2
 
 #Tratamiento variable dicótoma property_type
 
 #train
 
 #df <- df %>%
-  #mutate(property_typed = ifelse(df$property_type == "Casa", 1, 0))
+#mutate(property_typed = ifelse(df$property_type == "Casa", 1, 0))
 
 df <- df %>%
-  mutate(property_type = case_when(property_type == "Apartamento" ~ 1,
-                                   property_type == "Casa" ~ 0)
-         )
+  mutate(property_type = case_when(property_type_2 == "apartamento" ~ 0,
+                                   property_type_2 == "Apartamento" ~ 0,
+                                   property_type_2 == "Casa" ~ 1,
+                                   property_type_2 == "casa" ~ 1)
+  )
 
 #test
 
 df_test <- df_test %>%
-  mutate(property_type = case_when(property_type == "Apartamento" ~ 1,
-                                   property_type == "Casa" ~ 0)
+  mutate(property_type = case_when(property_type_2 == "apartamento" ~ 0,
+                                   property_type_2 == "Apartamento" ~ 0,
+                                   property_type_2 == "Casa" ~ 1,
+                                   property_type_2 == "casa" ~ 1)
   )
 
+#Verificación variable bedrooms
+
+
+
+#Variable Piso para apartamentos
+
+
+
+
+##Algoritmo detección áreas:  #1. Eliminar todos los espacios. 
+                              #2. Detectar todos los casos donde \d\d\d\d(mts) | \d\d\d(mts) | \d\d(mts) | \d\d\d\d(m2) | \d\d\d(m2) | \d\d(m2) | \d\d\d\d(metros) | \d\d\d(metros) | \d\d\(metros) | \d\d\d\d(metroscuadrados) | \d\d\d(metroscuadrados) | \d\d(metroscuadrados) | \d\d\d\d(metros2) | \d\d\d(metros2) | \d\d(metros2) 
+                              #3. Crear una columna donde se almacenen esos datos, con letras. 
+                              #4. Detectar en esa nueva columna solamente \d\d\d\d | \d\d\d | \d\d. 
+                              #5. Almacenar esos datos numéricos en otra nueva columna. 
+                              #6. Reemplazar datos en la columna de superficie (¿ambas superficie cubierta y total?), solamente si el valor es faltante.
+
+
+#Algoritmo detección de baños: (¿La ñ se toma como caracter especial?)  
+                              #1. Eliminar todos los espacios.
+                              #2. Detectar todos los casos donde \d\d(baos) | \d(baos) | \d(bao) | \d\d(banos) | \d(banos) | \d(bano) | \w\w\w\w\w\w\w(banos) | \w\w\w\w\w\w(banos) | \w\w\w\w\w(banos) | \w\w\w\w(banos) |\w\w\w(banos) | \w\w(banos) | \w\w(bano) \w\w\w\w\w\w\w(banos) | \w\w\w\w\w\w(banos) | \w\w\w\w\w(banos) | \w\w\w\w(banos) |\w\w\w(banos) | \w\w(banos) | \w\w(bano)
+                              #3. Crear una columna donde se almacenen esos datos, con letras.
+                              #4. Para baños con números: Detectar en esa nueva columna solamente \d\d | \d
+                              #5. Para baños con números:Almacenar esos datos numéricos en otra nueva columna.
+                              #6. Para baños con números: Reemplazar datos en la columna de baños, solamente si el valor falta.
+                              #7. Para baños con letras: Detectar en esa nueva columna solamente (baos) | (bao) | (banos) |(bao)
+                              #8. Eliminar de toda la columna las expresiones anteriormente dichas.
+                              #9. Transliterar cada palabra a número (reemplazar un = 1, dos = 2). O As.numeric?
+                              #10. Reemplazar datos en la columna de baños.
+
+#¿Qué hacer con rooms?
+
+#Variable de pisos para apartamentos. ¿Relación cuadrática?
 ##Receta 1, OLS
 
 rec1 <- recipe(price ~ surface_total + surface_covered + rooms + bedrooms + bathrooms + property_type, data = df)
